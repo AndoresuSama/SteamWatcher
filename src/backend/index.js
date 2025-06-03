e076696bb1a26365b82a89d1aebd0a58bdeb8429
andresg <andresg@fktech.net>
2025-06-03 14:34:32 -0300

    Implementación de CORS

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,25 +1,40 @@
-const http = require('http');
+const http = require("http");
 const { getList, deleteGame } = require("./routes/games");
 const { createGame, updateGames } = require("./routes/steamFetcher");
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
   try {
-    if (req.method === 'GET' && req.url.includes('/api/games') || req.url.includes('/api/games/updated')) {
-      if (req.url === '/api/games') await getList(req, res);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Manejar solicitudes OPTIONS (preflight)
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
+    if (
+      (req.method === "GET" && req.url.includes("/api/games")) ||
+      req.url.includes("/api/games/updated")
+    ) {
+      if (req.url === "/api/games") await getList(req, res);
       else await updateGames(req, res);
-    } else if (req.method === 'POST' && req.url == '/api/games') {
+    } else if (req.method === "POST" && req.url == "/api/games") {
       await createGame(req, res);
-    } else if (req.method === 'DELETE' && req.url.startsWith('/api/games/')) {
+    } else if (req.method === "DELETE" && req.url.startsWith("/api/games/")) {
       await deleteGame(req, res);
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'Not Found' }));
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Not Found" }));
     }
   } catch (error) {
-    res.end(JSON.stringify({ error }));
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: error.message }));
   }
 });
 
